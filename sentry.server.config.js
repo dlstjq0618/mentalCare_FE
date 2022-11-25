c032b070d3d2367cbf9d0431c6424e26d6d7e03a
dlstjq0618 <63532201+dlstjq0618@users.noreply.github.com>
2022-11-25 13:54:27 +0900

    .
    .

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -7,7 +7,9 @@ import * as Sentry from "@sentry/nextjs";
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn: SENTRY_DSN || "",
+  dsn:
+    SENTRY_DSN ||
+    "https://c0bb723feb794229a3cde9815d1d5ca8@o1176387.ingest.sentry.io/6274178",
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   // ...
