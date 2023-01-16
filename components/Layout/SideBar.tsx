a489e3355b6a75e136284f64680146afd4e3b3bf
dlstjq0618 <63532201+dlstjq0618@users.noreply.github.com>
2023-01-16 16:34:09 +0900

    ..
    ..

--- components/Layout/SideBar.tsx
+++ components/Layout/SideBar.tsx
@@ -109,7 +109,7 @@ const SideBar = (props: { total?: number; doctorName?: string }) => {
   const account_list = useSelector(selectAccoutList);
   const conference_list = useSelector(selectConferenceList);
 
-  const watingRoom_count = consultingList?.count + reservationList?.count + waitingList?.count + account_list?.count;
+  const watingRoom_count = consultingList?.count + reservationList?.count;
 
   const audioRef = useRef<HTMLAudioElement>(null);
   const shouldNotificate = useSelector(selectShouldNotificate);
